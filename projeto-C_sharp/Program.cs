84037aaf9737d2354722df8f4e3e220e05594884
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2021-03-25 20:34:24 +0000

    criado as classes

--- projeto-C_sharp/Program.cs
+++ projeto-C_sharp/Program.cs
@@ -9,4 +9,44 @@ namespace projeto_C_sharp
             Console.WriteLine("Hello World!");
         }
     }
+    class CaboVerde
+    {
+        
+    }
+    class SantoAntao
+    {
+
+    }
+    class SaoVicente
+    {
+
+    }
+    class SaoNicolau
+    {
+
+    }
+    class Sal
+    {
+
+    }
+    class Boavista
+    {
+
+    }
+    class Maio
+    {
+
+    }
+    class Santiago
+    {
+
+    }
+    class Brava
+    {
+
+    }
+    class Fogo
+    {
+        
+    }
 }
