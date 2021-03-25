06918ddd295f1d0df43186d7fc982970fb0aed69
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2021-03-25 20:54:41 +0000

    construido os metodo para a estrutura

--- projeto-C_sharp/Program.cs
+++ projeto-C_sharp/Program.cs
@@ -7,46 +7,100 @@ namespace projeto_C_sharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            menu("Lista", "sjdk", "njisj", "jdjdk");
+            leiaOp(1, 4);
         }
+        static void linha()
+        {
+            Console.WriteLine("------------------------------");
+        }
+        static void center(string teste, int num)
+        {
+            int total, esquerda, direita;
+            string test = "";
+            total = num - teste.Length;
+            direita = (total / 2) + teste.Length;
+            esquerda = num - direita;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0}{1}", teste.PadLeft(direita, ' '), test.PadRight(esquerda-1, ' '));
+            Console.ResetColor();
+        }
+        static void cabecalho(string msg)
+        {
+            linha();
+            center(msg, 30);
+            linha();
+        }
+        static void menu(params string[]lista)
+        {
+            int c = 1;
+            linha();
+            Console.WriteLine("Escolha sua opção: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            foreach (var item in lista)
+            {
+                Console.WriteLine($"[{c}] - {item}");
+                c++;
+            }
+            Console.ResetColor();
+            linha();
+        }
+        static int leiaOp(int inf, int sup)
+        {
+            int op;
+            while (true)
+            {
+                Console.Write("Sua opção: ");
+                op = Convert.ToInt32(Console.ReadLine());
+                if(op >= inf && op <= sup)
+                {
+                    return op;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"ERR!!! Digite um número entre {inf} a {sup}");
+                Console.ResetColor();
+            }
+        }
+
     }
     class CaboVerde
     {
         
     }
-    class SantoAntao
+    class SantoAntao : CaboVerde
     {
 
     }
-    class SaoVicente
+    class SaoVicente : CaboVerde
     {
 
     }
-    class SaoNicolau
+    class SaoNicolau : CaboVerde
     {
 
     }
-    class Sal
+    class Sal : CaboVerde
     {
 
     }
-    class Boavista
+    class Boavista : CaboVerde
     {
 
     }
-    class Maio
+    class Maio : CaboVerde
     {
 
     }
-    class Santiago
+    class Santiago : CaboVerde
     {
 
     }
-    class Brava
+    class Brava : CaboVerde
     {
 
     }
-    class Fogo
+    class Fogo : CaboVerde
     {
-        
+
     }
 }
